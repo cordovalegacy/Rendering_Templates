003a27a3043a07ad95a45ba7d1619e0b055e527a
Brendan Cordova <cordovalegacy19@gmail.com>
2023-01-04 10:59:20 -0800

    Practice with Jinja Basics

--- server.py
+++ server.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/hello/<int:num>/<name>')
+def hello(num, name):
+    return render_template('hello.html', num=num, name=name)
+
 if __name__=='__main__':
     app.run(debug = True)
 
@@ -12,3 +16,5 @@ if __name__=='__main__':
 
 ###pipenv install flask
 ###pipenv shell
+
+##In our template html files we are using a *template engine* called Jinja
